e2c849a935eab80d0b44931459a7bae4ed13bcc2
javier <javier.lozher@educa.jcyl.es>
2025-01-13 11:38:00 +0100

    Consultas Api

---
+++ app/src/main/java/com/psp/data/AlumnoService.kt
@@ -0,0 +1,27 @@
+package com.psp.data
+
+import com.psp.model.Alumno
+import retrofit2.Response
+import retrofit2.http.Body
+import retrofit2.http.DELETE
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+interface AlumnoService {
+
+    @GET("alumnos")
+    suspend fun requestAlumnos() : Response<List<Alumno>>
+
+    @GET("alumnos/curso/{curso}")
+    suspend fun requestAlumnosByCurso(@Path("curso") curso: String): Response<List<Alumno>>
+
+    @GET("alumnos/nombre/{nombre}")
+    suspend fun requestAlumnoByNombre(@Path("nombre") nombre: String): Response<Alumno>
+
+    @POST("alumnos")
+    suspend fun addAlumno(@Body alumno: Alumno)
+
+    @DELETE("alumnos/eliminar/{idAlumno}")
+    suspend fun deleteAlumnoById(@Path("idAlumno") idAlumno: Int)
+}
