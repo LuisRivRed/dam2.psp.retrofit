e249009740087c29517a66b9d940edcf93fe609f
Rubén Vallejo <146753439+Rubenvalleho@users.noreply.github.com>
2025-01-28 16:11:10 +0100

    Correguido

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,6 +68,8 @@ dependencies {
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.gson)
     implementation(libs.converter.gson)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+    implementation(libs.okhttp)
 
 
     testImplementation(libs.junit)
