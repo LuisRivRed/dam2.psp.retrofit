9b48226a799a9459a1930b28fb3caef2a7ba04ca
Diego Hernández Rubiano <diego.herrub@educa.jcyl.es>
2025-01-28 15:23:11 +0100

    wip - ApiService Test

--- app/src/main/java/com/psp/retrofit/data/remote/ApiClient.kt
+++ app/src/main/java/com/psp/retrofit/data/remote/ApiClient.kt
@@ -5,12 +5,21 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 object ApiClient {
 
-    //private const val BASE_URL_API = "http://127.0.0.1:8080/"
-    private const val BASE_URL_API = "http://192.168.32.1:8080/"
+
+    //private const val BASE_URL_API = "http://192.168.32.1:8080/" // casa cable
+
+    //private var baseUrl: String = "http://192.168.18.112:8080/" // clase clable
+
+    //private var baseUrl: String = "http://10.0.2.2:8099/" // clase clable
+    private var baseUrl: String = "http://127.0.0.1:51699/" // clase clable
+
+    fun setBaseUrlFromPreferences(prefUrl: String) {
+        baseUrl = prefUrl
+    }
 
     fun provideRetrofit(): Retrofit {
         return Retrofit.Builder()
-            .baseUrl(BASE_URL_API)
+            .baseUrl(baseUrl)
             .addConverterFactory(GsonConverterFactory.create())
             .build()
     }
