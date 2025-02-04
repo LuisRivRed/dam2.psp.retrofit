bd7d1b547cd6fe7553971da1e9522dc87d6e6471
heennaarr <henar.herher@educa.jcyl.es>
2025-02-04 23:58:56 +0100

    push

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,8 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.serialization)
+
 }
 
 android {
@@ -71,4 +73,6 @@ dependencies {
     implementation(platform(libs.okhttp.bom))
     implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
+    implementation(libs.retrofit2.kotlinx.serialization.converter)
+
 }
--- app/src/main/java/com/psp/data/ApiClient.kt
+++ app/src/main/java/com/psp/data/ApiClient.kt
@@ -1,13 +1,15 @@
 package com.psp.data
 
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import okhttp3.MediaType.Companion.toMediaType
 
-object ApiClient {  //
+
+object ApiClient {
     private const val BASE_URL = "http://10.0.2.2:8080/"
     private val authInterceptor = AuthInterceptor()
-
     private val okHttpClient = OkHttpClient
         .Builder()
         .addInterceptor(authInterceptor)
@@ -17,14 +19,14 @@ object ApiClient {  //
         ignoreUnknownKeys = true
         coerceInputValues = true
     }
-
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
         .client(okHttpClient)
+        .addConverterFactory(
+            json.asConverterFactory("application/json".toMediaType())
+        )
         .build()
-
     val apiService: ApiService = retrofit.create(ApiService::class.java)
-
     fun setToken(token: String) {
         authInterceptor.setToken(token)
     }
